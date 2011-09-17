7025b504c1c6f7e9a0098dd0f97a054b505cec5d
Prateek Jain <prateek.ja@gmail.com>
2011-09-16 22:48:41 -0400

    Added GridStructure and Column Parser
    GridStructure provides a neat and easy way for the developer to add
    properties for each column. The class discovers the columns in the
    LinqToSql class provided to it. In the future I plan to introduce a
    interface for providing data as well as using the present functionality
    of using the LinqToSql class.
    
    ColumnParser as the name suggests parses the names of the column and
    applies rules. These rules basically identify the Id columns and breaks
    up the column names based on capitals.

---
+++ source/MVCDojoGrid/ColumnParser.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcDojoGrid
+{
+    internal class ColumnParser
+    {
+        internal string[] Parse(string text)
+        {
+            if (text.Length == 0)
+                return null;
+
+            List<string> words = new List<string>();
+            //Rule1 - Remove UnderScore
+            string[] temp = text.Split('_');
+
+
+            //Rule2 - Seperate words based on Capitals
+            foreach (string s in temp)
+            {
+                if (s == "ID")  //Special case where the word is ID, its left out
+                { 
+                    words.Add(s);
+                    continue;
+                }
+                
+                bool wordBreak;
+                int wordStart = 0;
+
+                if (s.ToLower()[0] == s[0])
+                    wordBreak = false;
+                else
+                    wordBreak = true;
+
+                for (int i = 1; i < s.Length; i++)
+                {
+                    if ((s.ToUpper()[i] == s[i]) && (wordBreak == true) && (s.ToUpper()[i-1] != s[i-1]))
+                    {
+                        words.Add(s.Substring(wordStart, i - wordStart));
+                        wordStart = i;
+                        wordBreak = true;
+                    }
+                    else if ((s.ToUpper()[i] == s[i]) && (wordBreak == false))
+                        wordBreak = true;
+                }
+                words.Add(s.Substring(wordStart, s.Length - wordStart));
+            }
+            return words.ToArray();
+        }
+
+        internal string ColumnName(string text)
+        {
+            string[] words = Parse(text);
+            if (words == null)
+                return "";
+            StringBuilder name = new StringBuilder();
+            foreach (string word in words)
+            {
+                name.Append(word + " ");
+            }
+            return name.ToString().TrimEnd(' ');
+        }
+    }
+}
---
+++ source/MVCDojoGrid/GridStructure.cs
@@ -0,0 +1,124 @@
+﻿//GridStructure.cs
+//Author - Prateek Jain, Light Speed Solutions
+//prateek.jain@lightspeedsolutions.com
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Reflection;
+using System.Web;
+
+namespace MvcDojoGrid
+{
+    /// <summary>
+    /// Constructs the structure for the Dojo Grid
+    /// </summary>
+    public class GridStructure
+    {
+        private List<Dictionary<String, String>> _structure;
+        private PropertyInfo[] _props;
+        private string _idColumn;
+        private string _linkedColumn;
+
+        public GridStructure() { }
+
+        /// <summary>
+        /// Creates the grid structure with all the columns in the entity
+        /// </summary>
+        /// <param name="entity">The enity for which the structure has to be generated</param>
+        public GridStructure(Type entity)
+        {
+            
+            _structure = new List<Dictionary<string, string>>();
+            
+            PropertyInfo[] props = entity.GetProperties();
+            int i = 0;
+            _props = props;
+            foreach(PropertyInfo property in props)
+            {
+                i++;
+                Dictionary<String, String> cell = new Dictionary<string, string>();
+                ColumnParser parser = new ColumnParser();
+                string name = parser.ColumnName(property.Name);
+                cell.Add("name", name);
+                cell.Add("field", property.Name);
+                cell.Add("width", "auto");
+                cell.Add("headerStyles", "font-weight:bold;");
+                if (name.Contains("id") || name.Contains("Id") || name.Contains("ID"))
+                    cell.Add("hidden", "true");
+                _structure.Add(cell);
+                if (i >= 15)
+                    break;
+            }
+        }
+
+        public GridStructure(Type entity, string linkedColumn = "", string idColumn = "", GridEditStyle editStyle = GridEditStyle.None) : this(entity)
+        {
+            if (editStyle == GridEditStyle.Linked)
+            {
+                ColumnParser parser = new ColumnParser();
+                _idColumn = parser.ColumnName(idColumn);
+                _linkedColumn = parser.ColumnName(linkedColumn);
+                //int columnNum = -1;
+                //foreach (PropertyInfo property in _props)
+                //{
+                //    columnNum++;
+                //    if (property.Name == _linkedColumn)
+                //        break;
+                //}
+
+                this[_linkedColumn].Add("get", "getLink");
+                this[_linkedColumn].Add("formatter", "formatEditLink");
+                //this[_idColumn].Add("hidden", "true");
+
+                var cookie1 = new HttpCookie("grid_id", idColumn);
+                var cookie2 = new HttpCookie("grid_link", linkedColumn);
+
+                HttpContext.Current.Response.AppendCookie(cookie1);
+                HttpContext.Current.Response.AppendCookie(cookie2);
+            }
+        }
+
+        public Dictionary<String, String>[] structure
+        {
+            get
+            {
+                return _structure.ToArray();
+            }
+        }
+
+        public Dictionary<string,string> this[string columnName]
+        {
+            get
+            {
+                foreach (Dictionary<string, string> dict in structure)
+                {
+                    if (dict["name"] == columnName)
+                        return dict;
+                }
+                return null;
+            }
+
+            set
+            {
+                int columnNum = -1;
+                foreach (Dictionary<string, string> dict in structure)
+                {
+                    columnNum++;
+                    if (dict["name"] == columnName)
+                        break;
+                }
+                _structure.Remove(_structure[columnNum]);
+                _structure.Add(value);
+            }
+        }
+    }
+
+    public enum GridEditStyle
+    {
+        Inline,
+        Linked,
+        None
+    }
+}
