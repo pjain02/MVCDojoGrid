f5797dbb531c9b6f068ca8cb81c068ae5617aa2a
Prateek Jain <prateek.ja@gmail.com>
2011-09-16 23:22:27 -0400

    Added Dynamic.cs and DataResult.cs
    Dynamic.cs is part of the Dynamic LINQ Query Library by Scott Gutherie.
    It is particularly useful in dynamic linq operations on IEnumberables.
    
    DataResult is the wrapper for json which basically wraps the json with
    properties as required by the action filters.

---
+++ source/MVCDojoGrid/DataResult.cs
@@ -0,0 +1,39 @@
+﻿//DataResult.cs
+//Author - Prateek Jain
+
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace MvcDojoGrid
+{
+    /// <summary>
+    /// Wraps the JsonResult class to provide operations particularly for the Dojo Grid
+    /// </summary>
+    public class DataResult : JsonResult
+    {
+        private IQueryable<Object> _data;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="data">Object that needs to be serialized into JSON</param>
+        /// <param name="behavior">The Content-Type (MIME)</param>
+        public DataResult(IQueryable<Object> data, JsonRequestBehavior behavior) { base.Data = data; base.JsonRequestBehavior = behavior; _data = data; }
+
+        /// <summary>
+        /// Overriden ExecuteResult which sets additional headers to the context
+        /// </summary>
+        /// <param name="context"></param>
+        public override void ExecuteResult(ControllerContext context)
+        {
+            base.ExecuteResult(context);
+            //base.Data = _data;
+            //base.JsonRequestBehavior = _behavior;
+            IQueryable<Object> test = (IQueryable<Object>)base.Data;
+
+            context.HttpContext.Response.AddHeader("Content-Range", context.HttpContext.Request.Headers["Range"] + "/" + _data.Count());
+        }
+    } 
+}
